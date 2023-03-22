db1283f81c29d3c0703eb1245a37606f1cc08288
unknown <spremic.katarina@gmail.com>
2023-03-22 17:59:19 +0100

    Initialize project structure

---
+++ src/main/java/com/IBTim4/CertificatesApp/CertificatesAppApplication.java
@@ -0,0 +1,13 @@
+package com.IBTim4.CertificatesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificatesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IBTim4/CertificatesApp/CertificatesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IBTim4.CertificatesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificatesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
