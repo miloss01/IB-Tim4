9c5686640d29f8bbff2b84bd3d52169a2e7767a6
SelenaMilutin <selena.milutin@gmail.com>
2023-04-03 21:05:32 +0200

    Add login

--- backend/src/main/java/com/IBTim4/CertificatesApp/appUser/controller/AppUserController.java
+++ backend/src/main/java/com/IBTim4/CertificatesApp/appUser/controller/AppUserController.java
@@ -1,13 +1,21 @@
 package com.IBTim4.CertificatesApp.appUser.controller;
 
 import com.IBTim4.CertificatesApp.appUser.AppUser;
+import com.IBTim4.CertificatesApp.appUser.dto.LoginDTO;
 import com.IBTim4.CertificatesApp.appUser.dto.RegistrationRequestDTO;
+import com.IBTim4.CertificatesApp.appUser.dto.TokenResponseDTO;
 import com.IBTim4.CertificatesApp.appUser.dto.UserExpandedDTO;
 import com.IBTim4.CertificatesApp.appUser.service.interfaces.IAppUserService;
+import com.IBTim4.CertificatesApp.auth.JwtTokenUtil;
 import com.IBTim4.CertificatesApp.exceptions.CustomExceptionWithMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +31,10 @@ import java.util.Optional;
 public class AppUserController {
 
     @Autowired
+    JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
     IAppUserService appUserService;
 
     @PostMapping(produces = "application/json", consumes = "application/json")
@@ -37,6 +49,37 @@ public class AppUserController {
         AppUser saved = appUserService.saveAppUser(new AppUser(userDTO));
 
         return new ResponseEntity<>(new UserExpandedDTO(saved), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody LoginDTO loginDTO) {
+        System.out.println("LOGIN");
+
+        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword());
+
+
+        Authentication authentication = authenticationManager.authenticate(authReq);
+        System.out.println(authentication);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+        String role = securityContext.getAuthentication().getAuthorities().toString();
+
+
+        Optional<AppUser> optionalAppUser= appUserService.findByEmail(loginDTO.getEmail());
+        if (!optionalAppUser.isPresent()){
+            throw new CustomExceptionWithMessage("User does not exist!", HttpStatus.NOT_FOUND);
+        }
+        AppUser user = optionalAppUser.get();
+
+        String token = jwtTokenUtil.generateToken(
+                loginDTO.getEmail(),
+                user.getRole(),
+                user.getId());
+
+        return new ResponseEntity<>(
+                new TokenResponseDTO(token, ""),
+                HttpStatus.OK
+        );
     }
 
 }
--- backend/src/main/java/com/IBTim4/CertificatesApp/auth/WebSecurityConfiguration.java
+++ backend/src/main/java/com/IBTim4/CertificatesApp/auth/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.IBTim4.CertificatesApp.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,6 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration {
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,14 +31,12 @@ public class WebSecurityConfiguration {
                 .authorizeRequests()
                 .antMatchers("/*").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/api/**").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/certificate/request").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/certificate/request/accept").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+                .antMatchers("/api/user/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         http.headers().frameOptions().disable();
         http.cors();
 
@@ -55,6 +56,7 @@ public class WebSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
+//        System.out.println("milica" + encoder.encode("milica"));
         return encoder;
     }
 
